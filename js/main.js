8dccf72d47f9bc4095a95809010f7f937badba45
amir suliman <amirusliman01@gmail.com>
2023-07-10 23:39:13 +0500

    Fourth commit

--- js/main.js
+++ js/main.js
@@ -6,4 +6,26 @@ $(document).ready(function (){
   $toggleCollapse.click(function () {
     $nav.toggleClass('collapse')
   });
+
+
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            div:true
+        },
+        600:{
+            items:3,
+            div:false
+        },
+        1000:{
+            items:5,
+            div:true,
+            loop:false
+        }
+    }
+})
 });
