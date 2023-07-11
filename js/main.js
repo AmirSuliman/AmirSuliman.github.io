3ca5e78b2d04f2553cc962636ad9bc381b0cf428
amir suliman <amirsuliman01@gmail.com>
2023-07-12 01:28:41 +0500

    Last Commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
+const responsive = {
+  0:{
+    items:1
+  },
+  320:{
+    items:1
+  },
+
+  560:{
+    items:2
+  },
+  960:{
+    items:3
+  }
+
+}
+
 $(document).ready(function (){
   $nav = $('.nav');
   $toggleCollapse = $('.toggle-collapse');
@@ -14,6 +31,14 @@ $(document).ready(function (){
     autoplayTimeout:3000,
     dots:false,
     nav:true,
-    navText:[$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')]
+    navText:[$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')],
+    responsive:responsive
+  });
+
+  // click scroll top
+  $('.move-up span').click(function() {
+    $('html,body').animate({
+      scrollTop:0
+    },1000);
   });
 });
